0f1e94add7e8cab3568de1d5d2732640e27038ba
yamamishunsuke <ys26247ys@gmail.com>
2020-11-13 07:42:17 +0900

    CheckBox、RadioGroupなどの配置 android studioに慣れる

--- app/src/main/java/to/msn/wings/hello/MainActivity.java
+++ app/src/main/java/to/msn/wings/hello/MainActivity.java
@@ -1,26 +1,62 @@
 package to.msn.wings.hello;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.util.Date;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-//        レイアウトを整えている。
+
+        CheckBox chk = findViewById(R.id.chk);
+        chk.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    public void onCheckedChanged(CompoundButton buttonView,
+                                                 boolean isChecked) {
+                        Toast.makeText(MainActivity.this,
+                                isChecked ? "メール送信をオン" : "メール送信をオフ",
+                                Toast.LENGTH_SHORT).show();
+                }
+        });
+
+        ToggleButton toggle = findViewById(R.id.toggle);
+        toggle.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    public void onCheckedChanged(CompoundButton buttonView,
+                                                 boolean isChecked) {
+                        Toast.makeText(MainActivity.this,
+                                isChecked ? "メール送信をオン" : "メール送信をオフ",
+                                Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RadioGroup rgroup = findViewById(R.id.rgroup);
+        rgroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radio = group.findViewById(checkedId);
+                Toast.makeText(MainActivity.this,
+                        String.format("「%s」が選択されました。", radio.getText()),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public void btnCurrent_onClick(View view) {
-        TextView txt = findViewById(R.id.txtResult);
-        txt.setText(new Date().toString());
+    public void btnSend_onClick(View view) {
+        EditText txtName = findViewById(R.id.txtName);
+        TextView txtResult = findViewById(R.id.txtResult);
+        txtResult.setText(String.format("こんにちは、%sさん！", txtName.getText()));
     }
 }
+
